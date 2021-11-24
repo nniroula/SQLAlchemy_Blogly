a57bb060286b29bbcfc65cb2982e35f687fdd4b5
nabin <nkbhaie2017@gmail.com>
2021-11-24 11:40:26 -0700

    add routes for edit, delete, and new user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db  # import SQLAlchemy instance and a method
 from models import User   # import the class form models.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "nabinbro"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app) 
- 
 app.config['SQLALCHEMY_ECHO'] = True  # this helps to display sql tables in ipython terminal with db.create_all()
 app.config['SQLALCHEMY_TRACK_MODIFIACTIONS'] = False    # To supress sqlAlchemy message in terminal
 
@@ -35,27 +34,14 @@ def home_page():
 def list_all_users_in_db():
     # GET /users
     # Show all users.
-    # Make these links to view the detail page for the user.   # this means when you click on the user's name, it has to take you details page
-        # Do it in html file
+    # Make these links to view the detail page for the user.   # this means when you click on the user's name, it has to take you to details page
     # Have a link here to the add-user form.  # this is to add user button, DONE in user_lising.html file
     
-    # first_name_from_form = request.form["new-user-first-name"]   # this comes from new_user.html form
-    # last_name_from_form = request.form["new-user-last-name"]
-    # image_url_from_form = request.form["image-url"]
-
-    # Create object of model class, User off of these values
-    # all_users_list = User(first_name = first_name_from_form, last_name = last_name_from_form , image_url = image_url_from_form)
-    # save them to database
-    # db.seesion.add(all_users_list) 
-    # db.session.commit()
-    # for value retireval use obj.query.get(id)
     get_values_from_db = User.query.all()  # this is a query to get all items in database
     # OR use Model_Class.query.get_or_404() b/c get returns none if it does not find any thing. this returns 404 page
-    
     return render_template("user_listing.html", get_values_from_db = get_values_from_db)  # now this one, we need links here
 
-
-@app.route("/users/new", methods=["GET", "POST"])  # use two methods and if method==GET-do something, if method==Post-do another thing
+@app.route("/users/new", methods=["GET", "POST"]) 
 def get_new_user():
     """add a new user """
     # GET /users/new 
@@ -71,40 +57,50 @@ def get_new_user():
         last_name_from_form = request.form["new-user-last-name"]
         image_url_from_form = request.form["image-url"]
         all_users_list = User(first_name = first_name_from_form, last_name = last_name_from_form , image_url = image_url_from_form)
-        db.seesion.add(all_users_list) 
+        db.session.add(all_users_list) 
         db.session.commit()
-        # get_values_from_db = User.query.all() 
+
         return redirect("/users")
     
-
-
+@app.route("/users/<int:user_id>") 
+def show_details_about_user(user_id):
     # GET /users/[user-id]
     # Show information about the given user.
-
     # Have a button to get to their edit page, and to delete the user.
+    singleuser = User.query.get_or_404(user_id)    # becuase User.query.get(user_id) can return None
 
-########################
-"""
-Make routes for the following:
+    return render_template("user_detail_page.html", singleuser = singleuser) 
 
+@app.route("/users/<int:user_id>/edit", methods=["GET", "POST"])
+def show_edit_page_for_a_user(user_id):
+    # GET /users/[user-id]/edit
+    # Show the edit page for a user.
+    #Have a cancel button that returns to the detail page for a user, and a save button that updates the user.
 
-GET /users/[user-id]
-Show information about the given user.
+    if request.method == "GET":
+        return render_template("user_edit_page.html") 
 
-Have a button to get to their edit page, and to delete the user.
+    # POST /users/[user-id]/edit
+    # Process the edit form, returning the user to the /users page.
+    if request.method == "POST":
+        if request.form["cancelling_input"] == 'cancelling':  # info come from name and value attribute
+            # Or if value of name attribute in request.form ( here, if 'cancelling_inpt' in request.form)
+            return redirect("/user/user_id") 
 
-GET /users/[user-id]/edit
-Show the edit page for a user.
+        if request.form["saving_input"] == 'saving':  # if a button is save, update information
+            first_name_update = request.form["update-first"]
+            last_name_update = request.form["update-last"]
+            image_url_update = request.form["update-url"]
+            update_list = User(first_name = first_name_update, last_name = last_name_update , image_url = image_url_update)
+            db.seesion.add(update_list) 
+            db.session.commit()
+            return redirect("/users")
 
-Have a cancel button that returns to the detail page for a user, and a save button that updates the user.
-
-POST /users/[user-id]/edit
-Process the edit form, returning the user to the /users page.
-
-POST /users/[user-id]/delete
-Delete the user.
-
-Add Testing
-Add python tests to at least 4 of your routes.
-
-"""
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    # Delete the user.
+    id = User.query.get(user_id) 
+    User.query.filter_by(id = user_id).delete()
+    db.session.commit()
+    
+    return redirect("/users") 
